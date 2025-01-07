d97a89a40c4eef22b417b868f93c35bc922f6cbe
Stilllee <orodae@gmail.com>
2025-01-07 10:41:46 +0900

    Feat(#8): ETCButton 컴포넌트 제작

---
+++ src/components/common/buttons/ETCButton.tsx
@@ -0,0 +1,28 @@
+import { type ButtonHTMLAttributes } from "react";
+
+interface ETCButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  state?: "default" | "activated";
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function ETCButton({
+  state = "default",
+  children,
+  className,
+  ...props
+}: ETCButtonProps) {
+  const stateStyles = {
+    default: "bg-gray-800",
+    activated: "bg-gray-400",
+  }[state];
+
+  return (
+    <button
+      className={`btn-base h-[44px] max-w-[132px] gap-[10px] rounded-[12px] text-body-2-normal font-semibold text-gray-200 hover:bg-gray-400 ${stateStyles} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
