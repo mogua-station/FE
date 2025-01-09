68fb0d92282f942c544e0601c12a6629d560d808
Stilllee <orodae@gmail.com>
2025-01-10 00:23:04 +0900

    Refactor(#13): 프로필 mock 데이터 적용

--- src/components/user-page/UserProfile.tsx
+++ src/components/user-page/UserProfile.tsx
@@ -1,17 +1,27 @@
-const FAKE_TAGS = ["태그1", "태그2", "태그3"];
+const MOCK_PROFILE = {
+  userId: 1,
+  nickname: "모과씨",
+  profileImg:
+    "https://cdn.pixabay.com/photo/2024/11/21/22/06/deer-9214838_640.jpg",
+  qualificationStatus: "verified",
+  bio: "안녕하세요, 모과입니다.",
+  userTagList: ["개발자", "프론트엔드", "협업"],
+};
 
 export default function UserProfile() {
+  const { nickname, profileImg, bio, userTagList } = MOCK_PROFILE;
+
   return (
     <article className='flex min-w-[343px] justify-between rounded-[20px] bg-gray-950 px-5 py-4 desktop:px-9 desktop:py-8'>
       <div>
         <span className='text-heading-2 font-semibold text-gray-100'>
-          닉네임
+          {nickname}
         </span>
         <p className='mb-4 mt-4 text-label-normal font-regular text-gray-400'>
-          한 줄 소개가 들어갑니다
+          {bio}
         </p>
         <ul className='flex gap-1'>
-          {FAKE_TAGS.map((tag, idx) => (
+          {userTagList.map((tag, idx) => (
             <li
               className='w-fit rounded-md bg-gray-600 px-1.5 py-[3px] text-caption-normal font-medium text-gray-300'
               key={`tag-${idx}`}
@@ -21,7 +31,11 @@ export default function UserProfile() {
           ))}
         </ul>
       </div>
-      <div className='size-16 rounded-full bg-gray-700' />
+      <img
+        src={`${profileImg != null ? profileImg : "/images/default_user_profile.png"}`}
+        alt='유저 이미지'
+        className='size-16 rounded-full object-cover'
+      />
     </article>
   );
 }
