55af9c559d8ae66cb2909ef03cb0714cd5638434
Stilllee <orodae@gmail.com>
2025-01-10 14:21:43 +0900

    Feat(#13): 유저 페이지 추가 및 네비게이션 링크 업데이트

---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default function UserPage() {
+  return (
+    <div className='flex h-full flex-1 flex-col bg-[#5CA3DF] p-4 tablet:bg-[#E5C07B] tablet:px-20 tablet:py-[52px] desktop:bg-[#7b9c66] desktop:py-[82px]'>
+      {/* 유저 프로필 */}
+
+      {/* 탭 목록 */}
+
+      {/* 모임 목록 */}
+    </div>
+  );
+}
