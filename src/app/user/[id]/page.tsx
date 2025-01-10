b0dde56f6abdf0af207de17d94613af28bbd91db
Stilllee <orodae@gmail.com>
2025-01-10 15:14:32 +0900

    Refactor(#13): 유저페이지 레이아웃 수정

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,11 +1,14 @@
 export default function UserPage() {
   return (
+    // 데스크탑 뷰 디자인 확정x -> 모바일, 태블릿만 구현
     <div className='flex h-full flex-1 flex-col bg-[#5CA3DF] p-4 tablet:bg-[#E5C07B] tablet:px-20 tablet:py-[52px] desktop:bg-[#7b9c66] desktop:py-[82px]'>
-      {/* 유저 프로필 */}
+      <div className='mx-auto flex w-full flex-col desktop:min-w-[1041px] desktop:max-w-[1200px]'>
+        {/* 유저 프로필 */}
 
-      {/* 탭 목록 */}
+        {/* 탭 목록 */}
 
-      {/* 모임 목록 */}
+        {/* 모임 목록 */}
+      </div>
     </div>
   );
 }
