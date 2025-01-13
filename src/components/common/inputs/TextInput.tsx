08ef6217dbbe82f11d664b29f880cc2758f3f316
joshuayeyo <chiikawayeyo@gmail.com>
2025-01-13 18:06:34 +0900

    Refactor(#2): TextInput react-hook-form으로 전환

--- src/components/common/inputs/TextInput.tsx
+++ src/components/common/inputs/TextInput.tsx
@@ -1,4 +1,6 @@
-import OutlineSecondaryButton from "../buttons/OutlineSecondaryButton";
+import React from "react";
+import { Controller } from "react-hook-form";
+import OutlineButton from "../buttons/OutlineButton";
 import VisibilityToggle from "./VisibilityToggle";
 import useAuthInput from "@/hooks/inputs/auth/useAuthInput";
 import {
@@ -8,62 +10,81 @@ import {
 } from "@/hooks/inputs/style";
 import { type TextInputProps } from "@/types";
 
-const CommonTextInput: React.FC<TextInputProps> = ({ isValid, ...props }) => {
-  const { inputType, isPasswordVisible, togglePasswordVisibility } =
-    useAuthInput(props);
+const CommonTextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
+  ({ control, name, rules, ...props }, ref) => {
+    return (
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field, fieldState: { error } }) => {
+          const { inputType, isPasswordVisible, togglePasswordVisibility } =
+            useAuthInput({ ...props, name });
 
-  const containerStyle = textInputContainerStyle(
-    props.error,
-    props.value,
-    isValid,
-  );
-  const hint = hintStyle(props.error, isValid, props.name);
+          const containerStyle = textInputContainerStyle(
+            error?.message,
+            field.value,
+            field.value && !error,
+          );
+          const hint = hintStyle(error?.message, field.value && !error, name);
+          return (
+            <div className='flex flex-col gap-[12px]'>
+              {props.label && (
+                <label className='select-none text-body-2-normal font-medium text-gray-300'>
+                  {props.label}
+                  {props.required && (
+                    <span className='ml-1 text-red-500'>*</span>
+                  )}
+                </label>
+              )}
+              <div className='flex flex-col gap-[8px]'>
+                <div className='flex items-center gap-[8px]'>
+                  <div className={containerStyle}>
+                    <input
+                      {...field}
+                      {...props}
+                      ref={(e) => {
+                        field.ref(e);
+                        if (typeof ref === "function") ref(e);
+                        else if (ref) ref.current = e;
+                      }}
+                      className={invisibleInputStyle}
+                      type={inputType}
+                      autoComplete='new-password'
+                    />
+                    {(name === "password" || name === "confirmPassword") && (
+                      <VisibilityToggle
+                        isPasswordVisible={isPasswordVisible}
+                        togglePasswordVisibility={togglePasswordVisibility}
+                      />
+                    )}
+                  </div>
+                  {props.layout === "1button" && (
+                    <OutlineButton>Label</OutlineButton>
+                  )}
+                  {props.layout === "2buttons" && (
+                    <>
+                      <OutlineButton>Label</OutlineButton>
+                      <OutlineButton>Label2</OutlineButton>
+                    </>
+                  )}
+                </div>
+                {(props.hint || error?.message) && (
+                  <p
+                    className={`text-label-normal font-medium ${hint} select-none`}
+                  >
+                    {error?.message || props.hint}
+                  </p>
+                )}
+              </div>
+            </div>
+          );
+        }}
+      />
+    );
+  },
+);
 
-  return (
-    <div className='flex flex-col gap-[12px]'>
-      {props.label && (
-        <label className='select-none text-body-2-normal font-medium text-gray-300'>
-          {props.label}
-          {props.required && <span className='ml-1 text-red-500'>*</span>}
-        </label>
-      )}
-      <div className='flex flex-col gap-[8px]'>
-        <div className='flex items-center gap-[8px]'>
-          <div className={containerStyle}>
-            <input
-              {...props}
-              className={invisibleInputStyle}
-              type={inputType}
-            />
-            {/* input 내의 버튼 배치 */}
-            {(props.name === "password" ||
-              props.name === "confirmPassword") && (
-              <VisibilityToggle
-                isPasswordVisible={isPasswordVisible}
-                togglePasswordVisibility={togglePasswordVisibility}
-              />
-            )}
-          </div>
-          {/* props.layout에 따른 버튼 표시 */}
-          {props.layout === "1button" && (
-            <OutlineSecondaryButton>Label</OutlineSecondaryButton>
-          )}
-          {props.layout === "2buttons" && (
-            <>
-              <OutlineSecondaryButton>Label</OutlineSecondaryButton>
-              <OutlineSecondaryButton>Label2</OutlineSecondaryButton>
-            </>
-          )}
-        </div>
-        {/* input field 아래에 나오는 hint, error 메세지에 대한 스타일입니다 */}
-        {props.hint || props.error ? (
-          <p className={`text-label-normal font-medium ${hint} select-none`}>
-            {props.error || props.hint}
-          </p>
-        ) : null}
-      </div>
-    </div>
-  );
-};
+CommonTextInput.displayName = "CommonTextInput";
 
 export default CommonTextInput;
