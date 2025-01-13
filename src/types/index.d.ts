5ae03d7aabfacdc3905dbc41f72c3c3c09896033
joshuayeyo <chiikawayeyo@gmail.com>
2025-01-13 17:56:42 +0900

    Feat(#2): Form, SelectBox 관련 타입 추가

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,55 @@
-export type TextInputProps = {
+import { type InputHTMLAttributes, type ReactNode } from "react";
+import { type RegisterOptions, type FieldValues } from "react-hook-form";
+
+// 공통 InputProps 정의
+export interface InputProps<T extends FieldValues = FieldValues> {
+  name: keyof T;
+  control: any;
+  rules?: RegisterOptions<T>;
   label: string;
-  type: "text" | "password" | "email"; // input 타입
-  name: string;
-  value: string;
-  placeholder: string;
-  layout?: "default" | "1button" | "2buttons"; // 1버튼, 2버튼 레이아웃
-  hint?: string; // 입력 힌트
-  error?: string; // 에러 메시지
+  placeholder?: string;
+  required?: boolean;
+  error?: string;
   disabled?: boolean;
   isValid?: boolean;
-  required?: boolean;
-  children?: React.ReactNode;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onButtonClick?: () => void;
-};
+  children?: ReactNode;
+}
+
+export interface SelectBoxProps<T extends FieldValues = FieldValues>
+  extends InputProps<T>,
+    Omit<React.SelectHTMLAttributes<HTMLSelectElement>, "onChange"> {
+  secondName?: keyof T;
+  options: Option[];
+  secondOptions?: Option[];
+  status?: "default" | "filled" | "error";
+  layout: "1col" | "2cols";
+  secondPlaceholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  secondOnChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  value: string | undefined;
+  secondValue?: string | undefined;
+}
+
+export interface Option {
+  label: string;
+  value: string;
+}
+
+export interface TextInputProps<T extends FieldValues = FieldValues>
+  extends InputProps<T>,
+    Omit<InputHTMLAttributes<HTMLInputElement>, "size" | "onChange" | "value"> {
+  hint?: string;
+  layout?: "1button" | "2buttons";
+  // react-hook-form 관련 속성 추가
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+// FormData 타입 정의
+export interface FormData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  예시?: string;
+  exampleSelect3?: string;
+}
