65f1056b770ecff1a8a97e9af8b56b887bff5464
jmj <wjsdncl2222@gmail.com>
2025-01-14 11:12:12 +0900

    Move(#69): useSelectedDateRange 폴더 변경

--- src/components/common/modals/CalendarModal.tsx
+++ src/components/common/modals/CalendarModal.tsx
@@ -6,8 +6,8 @@ import SolidButton from "../buttons/SolidButton";
 import Calendar from "../calendar/Calendar";
 import InfoIcon from "@/assets/images/icons/info.svg";
 import ResetIcon from "@/assets/images/icons/reset_thin.svg";
+import { useSelectedDateRange } from "@/hooks/calendar/useSelectedDateRange";
 import useModal from "@/hooks/useModal";
-import { useSelectedDateRange } from "@/hooks/useSelectedDateRange";
 
 const CALENDAR_MODAL_TITLE = "모집 기간";
 
