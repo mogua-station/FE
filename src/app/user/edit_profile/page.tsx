5a38acf13c41cebf040dbe6f1c03d11ab81642fd
Stilllee <orodae@gmail.com>
2025-01-16 15:02:05 +0900

    Style(#61): 데스크탑 뷰 디자인 수정

--- src/app/user/edit_profile/page.tsx
+++ src/app/user/edit_profile/page.tsx
@@ -7,7 +7,6 @@ import EditProfileForm from "@/components/edit-profile/EditProfileForm";
 import { userProfileApi } from "@/lib/userProfile";
 
 // TODO: NavBar 없는 레이아웃 설정 필요
-// TODO: 모바일, 태블릿 UI 구현 완료 (데스크탑 디자인 문의 중)
 export default function EditProfile() {
   const userId = Number(process.env.NEXT_PUBLIC_USER_ID); // TODO: 임시 로그인유저 ID 사용(스토어로 관리 예정)
 
@@ -31,7 +30,7 @@ export default function EditProfile() {
 
   return (
     <section className='flex h-full flex-1 flex-col items-center bg-gray-950'>
-      <div className='flex w-full flex-col items-center bg-gray-900 p-4 tablet:px-10 tablet:pb-6 desktop:my-20 desktop:max-w-[960px] desktop:rounded-[40px]'>
+      <div className='flex w-full flex-col items-center bg-gray-900 p-4 tablet:px-10 tablet:pb-6 desktop:my-20 desktop:max-w-[712px] desktop:rounded-[40px]'>
         <h2 className='w-full px-2 pb-4 text-body-1-reading font-medium text-gray-100 tablet:pt-10'>
           계정 정보
         </h2>
