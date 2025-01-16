e4ff7653b4f08ff91945d35f2540e409bc34091e
joshuayeyo <chiikawayeyo@gmail.com>
2025-01-16 18:20:13 +0900

    Feat(#14): Cookie 핸들링 훅 추가

---
+++ src/hooks/auths/useTokenState.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const getCookie = (name: string): string | null => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const cookieValue = parts.pop()?.split(";").shift();
+    return cookieValue || null;
+  }
+  return null;
+};
+
+const useCookie = (cookieName: string): string | null => {
+  const [cookieValue, setCookieValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    const cookie = getCookie(cookieName);
+    setCookieValue(cookie);
+  }, [cookieName]);
+
+  return cookieValue;
+};
+
+export const deleteCookie = (
+  name: string,
+  path: string = "/",
+  domain: string = "",
+) => {
+  document.cookie = `${name}=; Max-Age=-1; path=${path}; domain=${domain};`;
+};
+
+export default useCookie;
