86c9d7d3c78a14f10af22472bef0205ff49b729a
Stilllee <orodae@gmail.com>
2025-01-18 18:11:48 +0900

    Feat(#62): 리뷰 작성 페이지 수정
    - 루트 레이아웃 패딩 제거 후 페이지 컨테이너 여백 수정
    - 태블릿 뷰 이상에서 보여지는 상단바 추가

--- src/app/user/create_review/page.tsx
+++ src/app/user/create_review/page.tsx
@@ -1,13 +1,20 @@
+import DeleteIcon from "@/assets/images/icons/delete.svg";
 import CreateReviewForm from "@/components/create-reaview/CreateReviewForm";
 
 export default function CreateReview() {
   return (
     // TODO: NavBar 없는 레이아웃 적용 필요
-    <div className='flex justify-center'>
-      <div className='flex flex-1 flex-col bg-black px-4 py-3.5 desktop:max-w-[584px]'>
-        <h2 className='mb-10 mt-[32px] text-heading-2 font-medium text-gray-200'>
+    <div className='flex justify-center bg-black'>
+      <div className='mx-4 mb-3.5 flex flex-1 flex-col tablet:mx-20 tablet:mb-[171px] tablet:mt-4 desktop:mx-20 desktop:mb-[104px] desktop:mt-[96px] desktop:max-w-[960px]'>
+        <div className='hidden items-center justify-between border-b border-gray-900 px-5 py-2.5 *:text-gray-200 tablet:flex'>
+          <h2>리뷰 작성하기</h2>
+          <button type='button'>
+            <DeleteIcon className='size-6' />
+          </button>
+        </div>
+        <p className='mb-10 mt-[34px] text-heading-2 font-medium text-gray-200'>
           모과님 <br /> 이번 모임은 어땠나요?
-        </h2>
+        </p>
         <CreateReviewForm />
       </div>
     </div>
