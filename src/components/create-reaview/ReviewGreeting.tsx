e885c45b14b59b7f33fd63de62d1f367b492d9dc
Stilllee <orodae@gmail.com>
2025-01-22 23:08:23 +0900

    Feat(#62): 인사말에 유저정보 연결

--- src/components/create-reaview/ReviewGreeting.tsx
+++ src/components/create-reaview/ReviewGreeting.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import useUserStore from "@/store/auth/useUserStore";
+
 // TODO: 유저 정보 받아와서 모과님 대신 유저 이름 넣기
 export default function ReviewGreeting() {
+  const { user } = useUserStore();
+
   return (
     <p className='mb-10 mt-[34px] text-heading-2 font-medium text-gray-200'>
-      모과님 <br /> 이번 모임은 어땠나요?
+      {user?.name}님 <br /> 이번 모임은 어땠나요?
     </p>
   );
 }
