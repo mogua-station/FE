34fd25b35bedb3b674398e68253de20e961d88ab
Stilllee <orodae@gmail.com>
2025-01-23 15:48:01 +0900

    Feat(#62): 리뷰 작성 취소 모달 구현

--- src/components/create-reaview/BackButton.tsx
+++ src/components/create-reaview/BackButton.tsx
@@ -1,13 +1,28 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import CreateReviewCancelModal from "./CreateReviewCancelModal";
 import DeleteIcon from "@/assets/images/icons/delete.svg";
+import useUserStore from "@/store/auth/useUserStore";
+import modal from "@/utils/modalController";
 
 export default function BackButton() {
-  const router = useRouter();
+  const { user } = useUserStore();
+
+  const handleOpenModal = () => {
+    modal.open(
+      ({ close }) => (
+        <CreateReviewCancelModal userId={user!.userId} close={close} />
+      ),
+      {
+        hasCloseBtn: false,
+        isBottom: false,
+        disableOverlayClick: true,
+      },
+    );
+  };
 
   return (
-    <button type='button' onClick={() => router.back()}>
+    <button type='button' onClick={handleOpenModal}>
       <DeleteIcon className='size-6' />
     </button>
   );
