902ea40199f6f41e666ebbc702f7aaba3d6e8a8d
Stilllee <orodae@gmail.com>
2025-02-04 17:52:04 +0900

    Refactor(#132): 메타데이터 타입 지정

--- src/app/user/edit_profile/page.tsx
+++ src/app/user/edit_profile/page.tsx
@@ -1,8 +1,9 @@
+import { type Metadata } from "next";
 import AccountActionButtons from "@/components/edit-profile/AccountActionButtons";
 import ContactBanner from "@/components/edit-profile/ContactBanner";
 import EditProfileForm from "@/components/edit-profile/EditProfileForm";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "프로필 수정 | mogua",
   description: "계정 정보를 업데이트하고 프로필을 관리하세요.",
   robots: {
