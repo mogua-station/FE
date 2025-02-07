5e048e6ad279b8c77d566de34a0a48ecfeb6be1c
jmj <wjsdncl2222@gmail.com>
2025-02-07 17:46:21 +0900

    Refactor(#158): 메타 데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { type Metadata } from "next";
 import dynamic from "next/dynamic";
 import localFont from "next/font/local";
 import Providers from "./providers/Providers";
@@ -18,6 +19,33 @@ const pretendard = localFont({
   display: "swap",
   weight: "45 920",
 });
+
+export const metadata: Metadata = {
+  title: "모과 | 스터디 & 과외 매칭 플랫폼",
+  description:
+    "모과(mogua)는 스터디 및 과외 모임을 쉽게 만들고 참여할 수 있는 플랫폼입니다. 원하는 과목과 지역에 맞춰 최적의 학습 파트너를 찾아보세요.",
+  keywords:
+    "스터디, 과외, 스터디 모집, 과외 모집, 그룹 스터디, 온라인 스터디, 오프라인 스터디, 공부 모임, 학습, 교육, 과외 플랫폼",
+  openGraph: {
+    title: "모과 | 스터디 및 과외 매칭 플랫폼",
+    description:
+      "스터디와 과외 모임을 쉽고 빠르게 찾아보세요. 원하는 과목과 지역에 맞는 학습 파트너를 연결해드립니다.",
+    url: "https://mogua.vercel.app/",
+    type: "website",
+    locale: "ko_KR",
+    siteName: "mogua",
+    // images: [
+    //   {
+    //     url: "/images/meta.webp",
+    //     alt: "모과(mogua) - 스터디 및 과외 매칭 플랫폼",
+    //   },
+    // ],
+  },
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://mogua.vercel.app/",
+  },
+};
 
 export default function RootLayout({
   children,
