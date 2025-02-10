083d87e1b652c509b26154d1845356011858e1a9
Stilllee <orodae@gmail.com>
2025-02-10 19:14:47 +0900

    Style(#199): 프로필수정, 리뷰작성 페이지 레이아웃 수정

--- src/app/user/edit_profile/page.tsx
+++ src/app/user/edit_profile/page.tsx
@@ -7,7 +7,7 @@ import BackButton from "@/components/user/BackButton";
 
 export default function EditProfile() {
   return (
-    <section className='nav-mb header-mt flex h-full flex-1 flex-col items-center bg-gray-950'>
+    <section className='header-mt flex h-full flex-1 flex-col items-center bg-gray-950'>
       <div className='flex w-full flex-col items-center bg-gray-900 p-4 tablet:px-20 tablet:pb-6 tablet:pt-10 desktop:mb-[106px] desktop:mt-14 desktop:max-w-[712px] desktop:rounded-[40px]'>
         <div className='flex w-full items-start justify-between px-2 pb-4'>
           <h2 className='w-full text-body-1-reading font-medium text-gray-100'>
