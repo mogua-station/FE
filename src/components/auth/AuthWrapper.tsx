181622b876aeaff57e485f92fb22d6395445fc89
joshuayeyo <chiikawayeyo@gmail.com>
2025-02-11 15:29:20 +0900

    Refactor(#207): 로직 최적화

--- src/components/auth/AuthWrapper.tsx
+++ src/components/auth/AuthWrapper.tsx
@@ -1,30 +1,36 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, type ReactNode } from "react";
 
 export default function AuthWrapper({ children }: { children: ReactNode }) {
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
-    // 인증 상태 확인
-    const checkAuth = async () => {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/user/verify`,
-        {
-          method: "GET",
-          credentials: "include",
-        },
-      );
+    if (pathname === "/sign-in") return;
 
-      // 인증되지 않은 경우 리다이렉트
-      if (!res.ok) {
-        router.push("/sign-in");
+    const checkAuth = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/user/verify`,
+          {
+            method: "GET",
+            credentials: "include",
+          },
+        );
+
+        if (!res.ok) {
+          router.replace("/sign-in");
+        }
+      } catch (error) {
+        console.error("인증 확인 중 오류 발생:", error);
+        router.replace("/sign-in");
       }
     };
-    checkAuth();
-  }, [router]);
 
-  // 인증 완료 시 자식 컴포넌트 렌더링
+    checkAuth();
+  }, [router, pathname]);
+
   return <>{children}</>;
 }
