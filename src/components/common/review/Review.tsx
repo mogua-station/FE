96bed52fd34a20821ee8687fcc5ab5fa8b1a4403
ITHPARK <122344944+ITHPARK@users.noreply.github.com>
2025-02-12 14:16:45 +0900

    Typo(#152): 주석 제거

--- src/components/common/review/Review.tsx
+++ src/components/common/review/Review.tsx
@@ -22,7 +22,6 @@ export default function Review({ reviewInfo }: ReviewInfoProps) {
 
   const ratingArr = ["그냥그래요", "괜찮아요", "추천해요"];
 
-  //클릭 시 모임 상세로 이동
   const handleClickReview = useCallback(() => {
     if (reviewInfo.isMyReview) {
       //과외와 스터디가 다른 라우터를 사용하기 때문에 eventType으로 분기처리
