12b27ae8173f624de1113e5dfef5f6298b43250d
ITHPARK <122344944+ITHPARK@users.noreply.github.com>
2025-02-12 09:29:07 +0900

    Feat(#152): 모임 참여 탈퇴 API 수정

--- src/lib/meetDetail/meetDetailApi.ts
+++ src/lib/meetDetail/meetDetailApi.ts
@@ -39,9 +39,7 @@ export const fetchJoinMeet = async (id: number) => {
       `${process.env.NEXT_PUBLIC_BASE_URL}/meetups/${id}/join`,
       {
         method: "POST",
-        headers: {
-          Authorization: `Bearer ${document.cookie.replace(/(?:(?:^|.*;\s*)accessToken\s*=\s*([^;]*).*$)|^.*$/, "$1")}`,
-        },
+        credentials: "include",
       },
     );
 
@@ -74,9 +72,7 @@ export const fetchLeaveMeet = async (id: number) => {
       `${process.env.NEXT_PUBLIC_BASE_URL}/meetups/${id}/leave`,
       {
         method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${document.cookie.replace(/(?:(?:^|.*;\s*)accessToken\s*=\s*([^;]*).*$)|^.*$/, "$1")}`,
-        },
+        credentials: "include",
       },
     );
 
