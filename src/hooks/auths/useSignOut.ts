d133763e4b255fcaa384ac48087759e547cba757
Stilllee <orodae@gmail.com>
2025-02-12 14:41:31 +0900

    Refactor(#132): useSignOut 로직 수정

--- src/hooks/auths/useSignOut.ts
+++ src/hooks/auths/useSignOut.ts
@@ -1,10 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import useUserStore from "@/store/auth/useUserStore";
 
 const useSignOut = () => {
+  const router = useRouter();
   const clearUser = useUserStore((state) => state.clearUser);
 
   const handleSignOut = async () => {
-    clearUser();
+    try {
+      router.push("/");
+
+      setTimeout(() => {
+        clearUser();
+      }, 100);
+    } catch (error) {
+      console.error("로그아웃 중 에러 발생:", error);
+    }
   };
 
   return { handleSignOut };
